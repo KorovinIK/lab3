972aaf242a346a8bdd0893fa67d9060067f3b628
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 19:27:33 +0300

    code: функция ввода 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 using namespace std;
 
+
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
+
 int main()
 {
     //ввод
