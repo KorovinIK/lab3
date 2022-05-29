784529f3a8e23b2c5232bf3a72e4d82ebc0bce23
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 20:08:50 +0300

    code: add make 1

--- main.cpp
+++ main.cpp
@@ -56,27 +56,7 @@ int main()
     cerr << "Enter bin count ";
     cin >> bin_count;
     //расчет гистограммы
-    double min, max;
-    find_minmax(numbers, min, max);
-    vector<size_t> bins(bin_count,0);
-    double bin_size = (max-min)/bin_count;
-    for (size_t i=0;i<number_count;i++)
-    {
-        bool found = false;
-        for (size_t j=0;j<(bin_count - 1) && !found;j++)
-        {
-            auto lo = min + j * bin_size;
-            auto hi = min + (j+1) * bin_size;
-            if ((lo <= numbers[i]) && (numbers[i] < hi)){
-                bins[j]++;
-                found = true;
-            }
-        }
-        if (!found)
-        {
-            bins[bin_count - 1]++;
-        }
-    }
+    const auto bins = make_histogram(numbers, bin_count);
     //вывод гистограммы
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
