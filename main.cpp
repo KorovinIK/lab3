313aa4e5f36c8de6cf0c5ccd3dcec6576a09ea4a
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 20:07:42 +0300

    code: add make_histogram

--- main.cpp
+++ main.cpp
@@ -20,6 +20,30 @@ void find_minmax(const vector<double> numbers, double& min, double& max) {
             max = number;}
     }
 }
+vector<size_t> make_histogramm(const vector<double>& numbers, size_t bin_count){
+    double min, max;
+    find_minmax(numbers, min, max);
+    vector<size_t> bins(bin_count,0);
+    double bin_size = (max-min)/bin_count;
+    for (size_t i=0;i<number_count;i++)
+    {
+        bool found = false;
+        for (size_t j=0;j<(bin_count - 1) && !found;j++)
+        {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j+1) * bin_size;
+            if ((lo <= numbers[i]) && (numbers[i] < hi)){
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            bins[bin_count - 1]++;
+        }
+    }
+    return bins;
+}
 int main()
 {
     //ввод
