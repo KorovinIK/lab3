db9cd11eeeaa9c6cb4be951c96b0cb3bf0f809e2
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 22:24:29 +0300

    code: вывод заголовка и окончания svg

--- main.cpp
+++ main.cpp
@@ -85,6 +85,19 @@ void show_histogramm_text(const vector<size_t>& bins){
     }
 }
 
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
 int main()
 {
     //ввод
