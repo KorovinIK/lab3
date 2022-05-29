6f7f1fc76968a6319f1fcc531c04f3f012252ef8
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 20:02:48 +0300

    code: поиск мин и макс 2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ vector<double> input_numbers(size_t count) {
     }
     return result;
 }
-void find_minmax(vector<double> numbers, double& min, double& max) {
+void find_minmax(const vector<double> numbers, double& min, double& max) {
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers){
