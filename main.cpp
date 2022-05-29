d05247acedae8b1e40b1d79daaf7e0e9aa3ea22c
Shrimp (KorovinIK) <korovinik@mpei.ru>
2022-05-29 20:17:42 +0300

    code: расчет гистограммы 2

--- main.cpp
+++ main.cpp
@@ -98,41 +98,6 @@ int main()
     //расчет гистограммы
     const auto bins = make_histogram(numbers, bin_count);
     //вывод гистограммы
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-    size_t max_bin = bins[0];
-    for (size_t bin : bins){
-        if (max_bin < bin){
-            max_bin = bin;}
-    }
-    bool prov = false;
-    if (max_bin > MAX_ASTERISK){
-        prov = true;
-    }
-    vector<size_t> binsh(bin_count,0);
-    for(size_t i=0; i<bin_count; i++ ){
-        size_t height = bins[i];
-        if (prov == true){
-            height = MAX_ASTERISK * (static_cast<double>(bins[i]) / max_bin);
-            binsh[i] = height;
-        }
-        else{
-            binsh[i] = bins[i];
-        }
-    }
-    for(size_t i=0; i<bin_count; i++ ){
-        if (bins[i] < 100){
-            cout << ' ';}
-        if (bins[i] < 10){
-            cout << ' ';}
-	    cout << bins[i] << "|";
-        for (size_t j=0; j<binsh[i]; j++){
-            if((j == binsh[i-1]-1) && (binsh[i-1] != binsh[i+1])   ){cout << "^";}
-            else if((j == binsh[i+1]-1) && (binsh[i-1] != binsh[i+1])   ){cout << "v";}
-            else if((j == binsh[i+1]-1) && (binsh[i-1] == binsh[i+1])   ){cout << "N";}
-            else{cout  << "*";}
-        }
-        cout << endl;
-    }
+    show_histogram_text(bins);
     return 0;
 }
